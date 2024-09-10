9c9e74a35245ebd53455eef59c2162c3361ec9f7
actionproject-shree <shree@actionproject.net>
2024-09-10 12:32:25 -0700

    plot all projections for cosmo volume hydro

--- simulate/linux/swift/examples.SmallCosmoVolume.SmallCosmoVolume_hydro.sh
+++ simulate/linux/swift/examples.SmallCosmoVolume.SmallCosmoVolume_hydro.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 set -ueox pipefail
+project_dir=$(pwd)
+simulation_dir=$(dirname "$0")
+
 source "$(echo $0 | sed 's|\(.*\)/.*|\1|')/bootstrap.sh" ${1:-default} "default" $(dirname -- "$0")
+run_dir=$(pwd)
 
 # Get the initial conditions if they are not present.
 if [ ! -e small_cosmo_volume.hdf5 ]
@@ -14,4 +18,13 @@ start=$(date +%s)
 end=$(date +%s)
 echo "run completed in " $(( end - start ))  "seconds"
 
-python3 plotProjection.py 31
+cd $project_dir
+cd $simulation_dir
+last_snapshot_index=$(ls $run_dir/snapshots | grep hdf5 | tail -1 | cut -f2 -d '_' | cut -f1 -d '.'| xargs echo | sed 's/^0*//')
+N=$((last_snapshot_index))
+
+cd $run_dir
+for i in $(seq 1 ${N%.*});
+do
+    python3 plotProjection.py $i
+done
